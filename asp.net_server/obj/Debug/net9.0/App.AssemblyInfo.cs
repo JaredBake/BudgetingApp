7d4d375babb3b3b89063cc6f9bc3da58cfeb12da
Jared Bake <93339550+JaredBake@users.noreply.github.com>
2025-09-29 10:57:07 -0600

    stuff

--- asp.net_server/obj/Debug/net9.0/App.AssemblyInfo.cs
+++ asp.net_server/obj/Debug/net9.0/App.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("App")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8e17eafd7d51055d83264a3d747f91d8c816dc13")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+387138509447c0878bda5ee25c1cc971ad264780")]
 [assembly: System.Reflection.AssemblyProductAttribute("App")]
 [assembly: System.Reflection.AssemblyTitleAttribute("App")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
