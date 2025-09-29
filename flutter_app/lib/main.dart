0384a1948ed7f5c3990e9c0037e066e496fe950b
Jared Bake <93339550+JaredBake@users.noreply.github.com>
2025-09-29 12:09:52 -0600

    Update main.dart
    added semi colon

--- flutter_app/lib/main.dart
+++ flutter_app/lib/main.dart
@@ -1,10 +1,11 @@
 import 'dart:convert'; // For JSON encoding/decoding
-import 'pages/welcome.dart'
 
 import 'package:english_words/english_words.dart';
 import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
 import 'package:provider/provider.dart';
+
+import 'pages/welcome.dart';
 
 void main() {
   runApp(MyApp());
