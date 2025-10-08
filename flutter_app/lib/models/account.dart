bde04109ea3e5bb52f950ba21f3187efeadd668c
SantosLaprida <lapridasantos95@gmail.com>
2025-10-07 18:34:29 -0600

    Data object details

--- flutter_app/lib/models/account.dart
+++ flutter_app/lib/models/account.dart
@@ -3,26 +3,26 @@ import 'transaction.dart';
 import 'fund.dart';
 import 'accountType.dart';
 
-
 // abstract class Account
 
 abstract class Account {
-    final int accountId;
-    final String name;
-    final String accountType; 
-    final Money balance;
+  final int accountId;
+  final String name;
+  final AccountType accountType;
+  final Money balance;
+  List<Transaction> transactions;
 
+  Account({
+    required this.accountId,
+    required this.name,
+    required this.accountType,
+    required this.balance,
+    required this.transactions,
+  });
 
-    Account({
-        required this.accountId,
-        required this.name,
-        required this.accountType,
-        required this.balance
-        });
-
-    bool addTransaction(Transaction transaction);
-    bool removeTransaction(Transaction transaction);
-    List<Transaction> getTransactions();
-    Money getBalance();
-
+  bool addTransaction(Transaction transaction);
+  bool removeTransaction(Transaction transaction);
+  List<Transaction> getTransactions();
+  Money getBalance();
+  int getAccountId() => this.accountId;
 }
