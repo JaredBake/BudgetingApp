7315da6e4751ca4a131d12a505a9efaca65d9383
Santos Laprida <136110849+SantosLaprida@users.noreply.github.com>
2025-10-07 18:43:46 -0600

    Data object details (#15)
    * Data object details
    
    * Gitignore
    
    * Gitignore

--- flutter_app/lib/models/data.dart
+++ flutter_app/lib/models/data.dart
@@ -1,23 +1,34 @@
 import 'account.dart';
 import 'fund.dart';
 
-
 class Data {
-    List<Fund> funds;
-    List<Account> accounts;
+  List<Fund> funds;
+  List<Account> accounts;
 
-    Data({
-        required this.funds, 
-        required this.accounts
-        });
+  Data({required this.funds, required this.accounts});
 
-    List<Account> getAccounts() {
-        return this.accounts;
+  List<Account> getAccounts() {
+    return this.accounts;
+  }
+
+  List<Fund> getFunds() {
+    return this.funds;
+  }
+
+  bool addAccount(Account account) {
+    if (!accounts.any((a) => a.accountId == account.accountId)) {
+      accounts.add(account);
+      return true;
     }
+    return false;
+  }
 
-
-    List<Fund> getFunds() {
-        return this.funds;
+  Account? findAccount(int accountId) {
+    for (Account account in accounts) {
+      if (accountId == account.accountId) {
+        return account;
+      }
     }
-
+    return null;
+  }
 }
