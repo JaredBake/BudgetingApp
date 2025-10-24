0d27cecc7870c96b83083f484e1c9f39d52dba81
Jared Bake <93339550+JaredBake@users.noreply.github.com>
2025-10-24 10:54:25 -0600

    added tests

---
+++ flutter_app/test/fund_test.dart
@@ -0,0 +1,22 @@
+import 'package:flutter_application/models/fund.dart';
+import 'package:flutter_application/models/money.dart';
+import 'package:flutter_test/flutter_test.dart';
+
+void main() {
+  test('Check if adding money correctly', () {
+    // Arrange
+    var fund = Fund(name: 'Test Fund', goalAmount: Money(amount: 100, currency: 'USD'), currentAmount: Money(amount: 10, currency: 'USD'), targetDate: DateTime.now());
+    // Act
+    var total = fund.addMoney(Money(amount: 5, currency: 'USD'));
+    // Assert
+    expect(total.amount, 15);
+  });
+
+  test("Check creation of Fund", (){
+    var fund = Fund(name: 'Vacation', goalAmount: Money(amount: 2000, currency: 'USD'), currentAmount: Money(amount: 500, currency: 'USD'), targetDate: DateTime(2024, 12, 31));
+    expect(fund.name, 'Vacation');
+    expect(fund.goalAmount.amount, 2000);
+    expect(fund.currentAmount.amount, 500);
+    expect(fund.targetDate, DateTime(2024, 12, 31));
+  });
+}
---
+++ flutter_app/test/hello_test.dart
@@ -0,0 +1,10 @@
+import 'package:flutter_test/flutter_test.dart';
+
+void main() {
+  test('Check that tests are running properly', () {
+    var two = 1 + 1;
+
+
+    expect(two, 2);
+  });
+}
