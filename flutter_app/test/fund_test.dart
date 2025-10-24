f16503f56cfb3d9d0dd91e498af8b5b3048ac81c
Jared Bake <93339550+JaredBake@users.noreply.github.com>
2025-10-24 11:06:06 -0600

    added widget tests

--- flutter_app/test/fund_test.dart
+++ flutter_app/test/fund_test.dart
@@ -3,6 +3,10 @@ import 'package:flutter_application/models/money.dart';
 import 'package:flutter_test/flutter_test.dart';
 
 void main() {
+
+//   void setUp(dynamic Function() body) {
+    
+// }
   test('Check if adding money correctly', () {
     // Arrange
     var fund = Fund(name: 'Test Fund', goalAmount: Money(amount: 100, currency: 'USD'), currentAmount: Money(amount: 10, currency: 'USD'), targetDate: DateTime.now());
@@ -19,4 +23,9 @@ void main() {
     expect(fund.currentAmount.amount, 500);
     expect(fund.targetDate, DateTime(2024, 12, 31));
   });
+
+  // test("Check Delete Fund", (){
+  //   var fund = Fund(name: 'Emergency', goalAmount: Money(amount: 1000, currency: 'USD'), currentAmount: Money(amount: 200, currency: 'USD'), targetDate: DateTime(2024, 6, 30));
+
+  // });
 }
