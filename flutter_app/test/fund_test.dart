421539d1b1c054ad94cbad82533fcff0b8599bce
Jared Bake <93339550+JaredBake@users.noreply.github.com>
2025-10-25 21:52:02 -0600

    Update fund_test.dart

--- flutter_app/test/fund_test.dart
+++ flutter_app/test/fund_test.dart
@@ -7,7 +7,7 @@ void main() {
 //   void setUp(dynamic Function() body) {
     
 // }
-  test('Check if adding money correctly', () {
+  test('Valid check adding of money', () {
     // Arrange
     var fund = Fund(name: 'Test Fund', goalAmount: Money(amount: 100, currency: 'USD'), currentAmount: Money(amount: 10, currency: 'USD'), targetDate: DateTime.now());
     // Act
@@ -16,13 +16,18 @@ void main() {
     expect(total.amount, 15);
   });
 
-  test("Check creation of Fund", (){
-    var fund = Fund(name: 'Vacation', goalAmount: Money(amount: 2000, currency: 'USD'), currentAmount: Money(amount: 500, currency: 'USD'), targetDate: DateTime(2024, 12, 31));
-    expect(fund.name, 'Vacation');
+  test("Valid creation of Fund", (){
+    var fund = Fund(name: 'Test', goalAmount: Money(amount: 2000, currency: 'USD'), currentAmount: Money(amount: 500, currency: 'USD'), targetDate: DateTime(2024, 12, 31));
+    expect(fund.name, 'Test');
     expect(fund.goalAmount.amount, 2000);
     expect(fund.currentAmount.amount, 500);
     expect(fund.targetDate, DateTime(2024, 12, 31));
   });
+
+  //   test("Invalid creation of Fund", (){
+  //   var fund = Fund(name: 'Test', goalAmount: Money(amount: 2000, currency: 'USD'), currentAmount: Money(amount: 500, currency: 'USD'), targetDate: DateTime(2024, 12, 31));
+  //   // Expect error response
+  // });
 
   // test("Check Delete Fund", (){
   //   var fund = Fund(name: 'Emergency', goalAmount: Money(amount: 1000, currency: 'USD'), currentAmount: Money(amount: 200, currency: 'USD'), targetDate: DateTime(2024, 6, 30));
