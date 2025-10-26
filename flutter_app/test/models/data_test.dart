1431102e9d479484252c2e052524b4e5c3b0f8c5
Jared Bake <93339550+JaredBake@users.noreply.github.com>
2025-10-26 17:52:11 -0600

    Update data_test.dart

---
+++ flutter_app/test/models/data_test.dart
@@ -0,0 +1,32 @@
+// import 'package:flutter_application/models/TransactionType.dart';
+// import 'package:flutter_application/models/cash.dart';
+// import 'package:flutter_application/models/money.dart';
+// import 'package:flutter_application/models/transaction.dart';
+// import 'package:flutter_application/models/data.dart';
+// import 'package:flutter_application/models/account.dart';
+// import 'package:flutter_application/models/accountType.dart';
+// import 'package:flutter_application/models/fund.dart';
+// import 'package:flutter_test/flutter_test.dart';
+
+// void main() {
+//   late Account testAccount;
+//   late Data dataModel;
+//   late Fund testFund;
+
+//   setUp((){
+//     testAccount = Account(
+//       accountId: 1,
+//       name: "Test Account",
+//       accountType: AccountType.checking,
+//       balance: Money(amount: 500.0, currency: "USD"),
+//       transactions: []
+//     );
+//     testFund = Fund(fundId: 1, name: "Test Fund", totalAmount: 1000.0);
+//     dataModel = Data(funds: [testFund], accounts: [testAccount]);
+//   });
+//   group('Add Account Tests', () {
+//     test('Valid addAccount', (){
+
+//     });
+//   });
+// }
